178615b0b2231093005a166e5e6d73cd1eaaa2a1
Sylvain Rebaud <sylvain@automatic.com>
2015-05-11 10:35:22 -0700

    Allow to pass a custom json body matcher. A default is provided if nil. Instead of comparing serialized json which is not canonical, the matcher parses the body and compares json objects.
    Test Plan: Tested with AUTAPIClient
    
    Reviewers: justin, James, eric, robb
    
    Reviewed By: robb
    
    Differential Revision: https://phabricator.automatic.co/D5463

--- AUTTestKit/LSStubRequestDSL+AUTJSONAdditions.h
+++ AUTTestKit/LSStubRequestDSL+AUTJSONAdditions.h
@@ -8,7 +8,24 @@
 
 #import <Nocilla/Nocilla.h>
 
-typedef LSStubRequestDSL *(^RequestWithJSONBodyMethod)(id);
+/// Since json serializer typically don't output canonical versions of json,
+/// comparing the output of two json serialized string is non-deterministic.
+/// Instead, actual json should be parsed and the actual object compared
+/// with the expected json dictionary.
+/// The purpose of a BodyMatcher is to do exactly that but can be customized
+/// if additional work must be done prior to parsing body (such as unzipping
+/// data for example)
+///
+/// body (NSData): the actual body as sent by the sender
+/// expectedBody (NSDictionary): the expected json dictionary as defined in a spec
+typedef BOOL(^BodyMatcher)(NSData *body, NSDictionary *expectedBody);
+
+/// A method that returns the result of invoking
+/// BodyMatcher block with the actual body and the expected one.
+///
+/// expectedBody (NSDictionary): JSON expected object
+/// matcher (BodyMatcher): a block that can decide whether a body matches the expected body
+typedef LSStubRequestDSL *(^RequestWithJSONBodyMethod)(NSDictionary *expectedBody, BodyMatcher matcher);
 
 @interface LSStubRequestDSL (AUTJSONAdditions)
 
--- AUTTestKit/LSStubRequestDSL+AUTJSONAdditions.m
+++ AUTTestKit/LSStubRequestDSL+AUTJSONAdditions.m
@@ -8,18 +8,65 @@
 
 #import "LSStubRequestDSL+AUTJSONAdditions.h"
 
+#pragma mark - AUTCustomBodyMatcher
+
+@interface AUTCustomBodyMatcher : NSObject <LSMatcheable>
+
+@property (nonatomic) id expectedBody;
+@property (nonatomic, copy) BodyMatcher bodyMatcher;
+
+@end
+
+@implementation AUTCustomBodyMatcher
+
+- (instancetype)initWithExpectedBody:(id)body customBodyMatcher:(BodyMatcher)bodyMatcher {
+    self = [super init];
+    
+    if (self) {
+        _expectedBody = body;
+        _bodyMatcher  = bodyMatcher;
+    }
+    return self;
+}
+
+- (LSMatcher *)matcher {
+    // LSMatcher protocol is not exposed publicly so cast is needed
+    return (id)self;
+}
+
+- (BOOL)matchesData:(NSData *)data {
+    // pass the actual data and the expected body to custom body matcher block
+    return self.bodyMatcher(data, self.expectedBody);
+}
+
+@end
+
+#pragma mark - LSStubRequestDSL
+
 @implementation LSStubRequestDSL (AUTJSONAdditions)
 
 - (RequestWithJSONBodyMethod)withJSON {
-    return ^(id body) {
-        NSError *error;
-        NSData *data = [NSJSONSerialization dataWithJSONObject:body options:0 error:&error];
-
-        NSAssert(data != nil, @"Could not serialize %@, received error: %@", body, error.localizedDescription);
-
-        return self
-            .withHeaders(@{ @"Content-Type": @"application/json" })
-            .withBody(data);
+    return ^(id body, BodyMatcher bodyMatcher) {
+        if (bodyMatcher == nil) {
+            bodyMatcher = ^BOOL(NSData *body, NSDictionary* expectedBody) {
+                // parse body into dictionary object
+                NSError *error = nil;
+                NSDictionary *json = [NSJSONSerialization JSONObjectWithData:body options:0 error:&error];
+                if (json == nil || error != nil) {
+                    return NO;
+                }
+                
+                // compare objects
+                return [json isEqualToDictionary:expectedBody];
+            };
+            
+             return self
+                .withHeaders(@{ @"Content-Type": @"application/json" })
+                .withBody([[AUTCustomBodyMatcher alloc] initWithExpectedBody:body customBodyMatcher:bodyMatcher]);
+        } else {
+            return self
+                .withBody([[AUTCustomBodyMatcher alloc] initWithExpectedBody:body customBodyMatcher:bodyMatcher]);
+        }
     };
 }
 
